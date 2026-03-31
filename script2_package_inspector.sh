52d2b653a6a55f623985b6b4f74c64b22b65b091
muskan24bsa10261-coder <muskan.24bsa10261@vitbhopal.ac.in>
2026-03-31 22:35:52 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 2: FOSS Package Inspector
-# Author: [Your Name] | Roll: [Your Roll Number]
+# Author: Muskan Pathak | Roll: 24BSA10261
 # Course: Open Source Software | OSS Audit — Git
 # Description: Checks if a FOSS package is installed,
 #              displays its details, and prints a philosophy note.
